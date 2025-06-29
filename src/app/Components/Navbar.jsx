e0df6ec3ec5684cc3083d423ab1008cb7f658b1f
mehedihasanpatowary007 <hasanmdrifat77@gamil.com>
2025-06-29 18:30:18 +0600

    check active links

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,26 +1,27 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
 function Navbar() {
+  const navItems = [
+    { name: "Home", path: "/", id: 1 },
+    { name: "Services", path: "/services", id: 2 },
+    { name: "About", path: "/about", id: 3 },
+    { name: "Contact", path: "/contact", id: 4 },
+  ];
   const pathname = usePathname();
   if (!pathname.includes("dashboard")) {
     return (
       <nav className="bg-red-400 py-3">
         <ul className="flex justify-around w-1/2">
-          <li>
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/services"}>Services</Link>
-          </li>
-          <li>
-            <Link href={"/about"}>About</Link>
-          </li>
-          <li>
-            <Link href={"/contact"}>Contact</Link>
-          </li>
+         {
+          navItems.map((item) =>{
+            return (
+              <Link className={`${pathname === item.path ? "text-violet-500 font-bold" : ""}`} key={item?.id} href={item?.path}>{item?.name}</Link>
+            )
+          })
+         }
         </ul>
       </nav>
     );
